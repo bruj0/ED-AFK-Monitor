0fbeb25b193d7a6b2739085f03c6f94ebd86b677
PsiPab <197918334+PsiPab@users.noreply.github.com>
2025-03-07 15:40:12 +0000

    Update EjectCargo event
    - Check that only 1t of a commodity was ejected
    - Update language 'ejected' > 'stolen'

--- afk_monitor.py
+++ afk_monitor.py
@@ -340,10 +340,10 @@ def processevent(line):
 				logevent(msg_term=f'{Col.WARN}Pirate didn\'t engage due to insufficient cargo value{Col.END}',
 			 			msg_discord='**Pirate didn\'t engage due to insufficient cargo value**',
 						emoji='🎣', timestamp=logtime, loglevel=getloglevel('BaitValueLow'))
-		case 'EjectCargo' if not this_json["Abandoned"]:
+		case 'EjectCargo' if not this_json["Abandoned"] and this_json['Count'] == 1:
 			name = this_json['Type_Localised'] if 'Type_Localised' in this_json else this_json['Type'].title()
-			logevent(msg_term=f'{Col.BAD}Cargo ejected!{Col.END} ({name})',
-					msg_discord=f'**Cargo ejected!** ({name})',
+			logevent(msg_term=f'{Col.BAD}Cargo stolen!{Col.END} ({name})',
+					msg_discord=f'**Cargo stolen!** ({name})',
 					emoji='📦', timestamp=logtime, loglevel=getloglevel('CargoLost'))
 		case 'Missions' if 'Active' in this_json and not track.missions:
 			track.missionsactive.clear()
